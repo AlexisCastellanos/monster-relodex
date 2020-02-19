2c8b2320a87a980a93b7a74efd844baf51476ca7
Alexis Castellanos <intelaki@gmail.com>
2020-02-18 23:03:49 -0600

    Updates

---
+++ precache-manifest.18ac26ad551a07d877c403fa50304633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddfc482866e9d0f691498c558011f7a",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "c23b3c933e7cb54cb444",
+    "url": "/monster-relodex/static/css/main.92bc483a.chunk.css"
+  },
+  {
+    "revision": "f735ba4e00c7e4493a36",
+    "url": "/monster-relodex/static/js/2.c2a691c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-relodex/static/js/2.c2a691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23b3c933e7cb54cb444",
+    "url": "/monster-relodex/static/js/main.43fbd4c8.chunk.js"
+  },
+  {
+    "revision": "68446be86781ae2707c9",
+    "url": "/monster-relodex/static/js/runtime-main.6d3dbc3f.js"
+  }
+]);
